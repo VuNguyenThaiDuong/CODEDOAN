b8e6c8d4e2be1ac08349333f28f6bd361e8fcbcc
VuNguyenThaiDuong <161466508+VuNguyenThaiDuong@users.noreply.github.com>
2024-05-20 09:12:30 +0700

    Add files via upload

---
+++ #Docstring.py
@@ -0,0 +1,7 @@
+# Khai báo thư viện cẩn thiết
+# Đọc file và clear data
+# Encoder target label và scale data trước khi train
+# Chia train và test
+# Hàm train bằng chỉ số Gini
+# Hàm train bằng chỉ số Entropy
+# Train data
